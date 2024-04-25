1750ef914325400c9d8ea3bdcb732010149be8bf
isa44zy <162628718+isa44zy@users.noreply.github.com>
2024-04-25 16:37:24 -0300

    Projeto 25/04

---
+++ src/main/java/com/Avaliacao3/Avaliacao3Application.java
@@ -0,0 +1,13 @@
+package com.Avaliacao3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avaliacao3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avaliacao3Application.class, args);
+	}
+
+}
---
+++ src/main/java/repository/TurmaRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entities.Turma;
+
+public interface TurmaRepository extends JpaRepository<Turma,Long>{
+	List<Turma> findByNome(String nome);
+	List<Turma> findByDescricao(String descricao);
+	List<Turma> findByNomeAndDescricao(String nome, String descricao);
+}
